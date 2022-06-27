34e6b82549e53dfb32939c3139ae96167b4ce3aa
Galuta Dmitrii <dvgaluta@gmail.com>
2022-06-28 00:49:49 +0300

    commit_10

--- advertisements/views.py
+++ advertisements/views.py
@@ -1,15 +1,19 @@
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
-
+from .serializers import AdvertisementSerializer
+from .models import Advertisement
 
 class AdvertisementViewSet(ModelViewSet):
     """ViewSet для объявлений."""
 
     # TODO: настройте ViewSet, укажите атрибуты для кверисета,
     #   сериализаторов и фильтров
+    queryset = Advertisement.objects.all()
+    serializer_class = AdvertisementSerializer
 
     def get_permissions(self):
         """Получение прав для действий."""
         if self.action in ["create", "update", "partial_update"]:
             return [IsAuthenticated()]
         return []
+
