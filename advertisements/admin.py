6a365948a8c004c7f4e507e9891b6c2f2a493771
Galuta Dmitrii <dvgaluta@gmail.com>
2022-06-28 00:44:27 +0300

    commit_9

--- advertisements/admin.py
+++ advertisements/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Advertisement
 # Register your models here.
+@admin.register(Advertisement)
+class Advertisement(admin.ModelAdmin):
+    list_display = ['id', 'title', 'descriptions', 'status', 'creator', 'created_at', 'updated_at']
+    pass
+
+
